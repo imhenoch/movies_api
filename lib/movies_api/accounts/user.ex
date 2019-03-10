7d4d2c1b09669d3bc960194ef2d9147ff0143ee2
Isidro Henoch <isidro.henoch@gmail.com>
2019-03-09 21:24:04 -0600

    Changes the user schema to hash the password

--- lib/movies_api/accounts/user.ex
+++ lib/movies_api/accounts/user.ex
@@ -2,10 +2,11 @@ defmodule MoviesApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "users" do
     field :email, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
@@ -13,8 +14,22 @@ defmodule MoviesApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
     |> unique_constraint(:email)
+    |> put_password_hash
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
