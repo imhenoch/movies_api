dc02b9d9bdaa6d69202a54ea2d77a9ec6d72f239
Isidro Henoch <isidro.henoch@gmail.com>
2019-03-09 21:44:04 -0600

    Adds token to user view

--- lib/movies_api_web/views/user_view.ex
+++ lib/movies_api_web/views/user_view.ex
@@ -3,16 +3,28 @@ defmodule MoviesApiWeb.UserView do
   alias MoviesApiWeb.UserView
 
   def render("index.json", %{users: users}) do
-    %{data: render_many(users, UserView, "user.json")}
+    %{
+      data: render_many(users, UserView, "user.json")
+    }
   end
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, UserView, "user.json")}
+    %{
+      data: render_one(user, UserView, "user.json")
+    }
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id,
+    %{
+      id: user.id,
       email: user.email,
-      password_hash: user.password_hash}
+      password_hash: user.password_hash
+    }
+  end
+
+  def render("jwt.json", %{jwt: token}) do
+    %{
+      jwt: token
+    }
   end
 end
